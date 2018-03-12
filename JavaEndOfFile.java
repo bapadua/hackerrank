21f1a4370c4b54d6916927f719a8d2912f947f5c
Bruno <anjozero@gmail.com>
2018-03-12 01:24:15 -0300

    Java End-of-file
    https://www.hackerrank.com/challenges/java-end-of-file/problem

---
+++ JavaEndOfFile.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class JavaEndOfFile {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);   
+        int x=1;
+        while (sc.hasNextLine()) {
+        System.out.println(x + " " +sc.nextLine());
+        x++;
+        }
+    }
+}
